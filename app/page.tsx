ab260c00c0abc8383e6279fe1a2ddb156cc226e1
Alexander Kalinin <alex@kalinin.uk>
2024-09-28 12:11:23 +0100

    localstorage for file, remove styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
-import { useState } from "react"
+import { DiveData, parseFile } from "@/src/parseFile"
+import { useEffect, useState } from "react"
 
 const readJsonFile = (file: Blob) =>
   new Promise((resolve, reject) => {
@@ -8,7 +9,7 @@ const readJsonFile = (file: Blob) =>
 
     fileReader.onload = event => {
       if (event.target) {
-        resolve(JSON.parse(event.target.result as string))
+        resolve(event.target.result as string)
       }
     }
 
@@ -20,21 +21,37 @@ const readJsonFile = (file: Blob) =>
 
 export default function Home() {
 
-  const [diveCount, setDiveCount] = useState(0)
+  const [diveData, setDiveData] = useState<DiveData | undefined>(undefined)
 
   const onChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files) {
-      const parsedData = await readJsonFile(event.target.files[0]) as any
-      setDiveCount(parsedData.ScubaDive.length)
-      console.log(parsedData)
+      const json = await readJsonFile(event.target.files[0]) as string;
+      localStorage.setItem('diveData', json);
+      setDiveData(parseFile(json))
     }
   }
+
+  useEffect(() => {
+    const json = localStorage.getItem('diveData');
+    if (json != null) {
+      setDiveData(parseFile(json));
+    }
+  })
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start"></main>
       <h1 className="text-4xl font-bold">Cressi Dive Visualiser</h1>
-      <h2>Dives: {diveCount}</h2>
+      {diveData &&
+        <>
+          <h2>Dives: {diveData.ScubaDive.length}</h2>
+          <ul>
+            {diveData.ScubaDive.map(dive => (
+              <li key={dive.ID}>{dive.DiveStart}</li>
+            ))}
+          </ul>
+        </>
+      }
       <input type="file" accept=".json,application/json" onChange={onChange} />
     </div>
   );
