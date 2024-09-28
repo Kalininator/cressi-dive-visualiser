15f9e6f42f0834ec77079e2236b6f946595f9f4d
Alexander Kalinin <alex@kalinin.uk>
2024-09-28 12:36:53 +0100

    shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 'use client'
 
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 import { DiveData, parseFile } from "@/src/parseFile"
 import { useEffect, useState } from "react"
+import { toast } from "sonner"
 
 const readJsonFile = (file: Blob) =>
   new Promise((resolve, reject) => {
@@ -28,6 +31,7 @@ export default function Home() {
       const json = await readJsonFile(event.target.files[0]) as string;
       localStorage.setItem('diveData', json);
       setDiveData(parseFile(json))
+      toast("Loaded dive data from file");
     }
   }
 
@@ -35,11 +39,12 @@ export default function Home() {
     const json = localStorage.getItem('diveData');
     if (json != null) {
       setDiveData(parseFile(json));
+      toast("Loaded dive data from local storage");
     }
-  })
+  }, [])
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start"></main>
       <h1 className="text-4xl font-bold">Cressi Dive Visualiser</h1>
       {diveData &&
@@ -52,7 +57,10 @@ export default function Home() {
           </ul>
         </>
       }
-      <input type="file" accept=".json,application/json" onChange={onChange} />
+      <div className="grid w-full max-w-sm items-center gap-1.5">
+        <Label htmlFor="picture">Dives File</Label>
+        <Input type="file" accept=".json,application/json" onChange={onChange} />
+      </div>
     </div>
   );
 }
