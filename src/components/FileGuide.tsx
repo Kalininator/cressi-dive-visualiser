23b9f59ba29508d6cc50aa87d5843b17507f53fd
Alexander Kalinin <alex@kalinin.uk>
2024-10-05 23:22:49 +0100

    fix 2

--- src/components/FileGuide.tsx
+++ src/components/FileGuide.tsx
@@ -5,7 +5,7 @@ const FileGuide = () => {
 	return <div className="max-w-xl">
 		<h1 className="font-bold text-xl">Loading your data</h1>
 		This tool uses the data exported from the Cressi UCI desktop software.
-		To export your data, open the Cressi UCI software and click the "Backup" button.
+		To export your data, open the Cressi UCI software and click the Backup button.
 		<Image src={backupButton} alt="Cressi Backup Button" className="w-30" />
 	</div >;
 }
